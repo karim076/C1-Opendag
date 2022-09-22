b9649ddc00c1c2617c49844ef85194a689c7fb83
karim076 <59794989+karim076@users.noreply.github.com>
2022-09-22 18:28:33 +0200

    More text

--- index.php
+++ index.php
@@ -78,16 +78,28 @@
 				</p>
 			</div>
 			<div class="vakWidget generiek">
+				<h2>Pra</h2>
+				<p>Bij pra krijg je een grote project met een groep, het project kan web of native onderwerp hebben.</p>
+			</div>
+			<div class="vakWidget generiek">
 				<h2>Web</h2>
-				<p>Bij web ga je website's maken van frontend tot backend</p>
+				<p>Bij web ga je website's maken van frontend tot backend.</p>
 			</div>
 			<div class="vakWidget generiek">
 				<h2>Native</h2>
-				<p>Bij Native ga je het software crëeeren</p>
+				<p>Bij Native ga je het software crëeeren.</p>
 			</div>
 			<div class="vakWidget generiek">
 				<h2>Pro</h2>
-				<p>Bij Praktijkondersteuning leer je het voorbereiden van het maken van website en software</p>
+				<p>Bij Praktijkondersteuning leer je het voorbereiden van het maken van website en software.</p>
+			</div>
+			<div class="vakWidget generiek">
+				<h2>Pro</h2>
+				<p>Bij Praktijkondersteuning leer je het voorbereiden van het maken van website en software.</p>
+			</div>
+			<div class="vakWidget generiek">
+				<h2>Codelab</h2>
+				<p>Bij codelab krijg je een uurtje de tijd of je huiswerk te maken, je kunt dan de leraar vragen stellen als je vast zit.</p>
 			</div>
 		</div>	
 	</main>

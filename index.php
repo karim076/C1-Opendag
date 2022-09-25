b52d7607f3e01f16a1afbed6e7d46b9b44f883b4
karim076 <59794989+karim076@users.noreply.github.com>
2022-09-25 14:50:21 +0200

    Added comments and fixed JS bug hamburger menu and the cross show both on startt

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<header>
-		<div class="navbars">
+		<div class="navBars">
 			<div>
 				<ul class="menus">
 				    <li><a class="menuItem" href="#">Home</a></li>
@@ -27,24 +27,30 @@
 		    <a href="#" class="diff">contact</a>
 		</div>
 		<script type="text/javascript">
+			// Variablen aanmaken van klassen
 			const menu = document.querySelector(".menus");
 			const menuItems = document.querySelectorAll(".menuItem");
 			const hamburger= document.querySelector(".hamburger");
 			const closeIcon= document.querySelector(".closeIcon");
 			const menuIcon = document.querySelector(".menuIcon");
-
+			//Nu begint eerst het hamburger menu in plaats het kruis
+			menuIcon.style.display = "block";
+			closeIcon.style.display = "none";
+			// functie bij het aanklikken wordt dit aangeroepen
 			function toggleMenu() {
 			  if (menu.classList.contains("showMenu")) {
 			    menu.classList.remove("showMenu");
 			    closeIcon.style.display = "none";
 			    menuIcon.style.display = "block";
+			    hamburger.style.background = "transparent";
 			  } else {
 			    menu.classList.add("showMenu");
+			    hamburger.style.background = "white";
 			    closeIcon.style.display = "block";
 			    menuIcon.style.display = "none";
 			  }
 			}
-
+			// event listener wordt dan toggleMenu geroepen om uit te voeren
 			hamburger.addEventListener("click", toggleMenu);
 
 			menuItems.forEach( 
@@ -71,9 +77,9 @@
 				<div><h3>19:30 15-9-2022</h3></div>
 			</div>
 		</div>
-		<div class="centerText" id="vakken"><h1>→Vakken←</h1></div>
+		<div class="vakkenText" id="vakken"><h1>→Vakken←</h1></div>
 		<div class="vakken">
-			<div class="vakWidget generiek col-sm-12 col-md-6 col-lg-5 col-xl-4">
+			<div class="vakWidget  ">
 				<h2>Generiek</h2>
 				<p>━━━━━━━━━━━━━━━━━━━━━━━━━━━━</p>
 				<p>-Nederlands<br>
@@ -81,28 +87,28 @@
 					-Rekenen
 				</p>
 			</div>
-			<div class="vakWidget generiek col-sm-12 col-md-6 col-lg-5 col-xl-4">
+			<div class="vakWidget">
 				<h2>Pra</h2>
 				<p>━━━━━━━━━━━━━━━━━━━━━━━━━━━━</p>
 				<p>Bij pra krijg je een grote project met een groep, het 
 				project kan web of native onderwerp hebben.</p>
 			</div>
-			<div class="vakWidget generiek col-sm-12 col-md-6 col-lg-5 col-xl-4">
+			<div class="vakWidget">
 				<h2>Web</h2>
 				<p>━━━━━━━━━━━━━━━━━━━━━━━━━━━━</p>
 				<p>Bij web ga je website's maken van frontend tot backend.</p>
 			</div>
-			<div class="vakWidget generiek col-sm-12 col-md-6 col-lg-5 col-xl-4">
+			<div class="vakWidget">
 				<h2>Native</h2>
 				<p>━━━━━━━━━━━━━━━━━━━━━━━━━━━━</p>
 				<p>Bij Native ga je het software crëeeren.</p>
 			</div>
-			<div class="vakWidget generiek col-sm-12 col-md-6 col-lg-5 col-xl-4">
+			<div class="vakWidget">
 				<h2>Pro</h2>
 				<p>━━━━━━━━━━━━━━━━━━━━━━━━━━━━</p>
 				<p>Bij Praktijkondersteuning leer je het voorbereiden van het maken van website en software.</p>
 			</div>
-			<div class="vakWidget generiek col-sm-12 col-md-6 col-lg-5 col-xl-4">
+			<div class="vakWidget">
 				<h2>Codelab</h2>
 				<p>━━━━━━━━━━━━━━━━━━━━━━━━━━━━</p>
 				<p>Bij codelab krijg je een uurtje de tijd of je huiswerk te maken, je kunt dan de leraar vragen stellen als je vast zit.</p>
